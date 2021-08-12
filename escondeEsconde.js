30e6e4814f57372145fa583c9aa8ddc82f730075
Mike <11251303+mikedpsm@users.noreply.github.com>
2021-08-12 20:22:54 -0300

    Algoritmo ESCONDE ESCONDE

---
+++ escondeEsconde.js
@@ -0,0 +1,9 @@
+function solucao(numero) {
+    if (numero <= 20) {
+        console.log("VOCE CONSEGUE, MANINHO");
+    } else {
+        for (let i = 21; i <= numero; i++) {
+            console.log(i);
+        }
+    }
+}
