027f0f5a52942369d1120d3e87a3fe8c0e844874
Mike <11251303+mikedpsm@users.noreply.github.com>
2021-08-12 20:22:06 -0300

    Algoritmo CORES

---
+++ Cores.js
@@ -0,0 +1,17 @@
+function solucao(cor1, cor2) {
+    if (cor1 === cor2) {
+        console.log(cor1);
+    } else if (cor1 === "azul" && cor2 === "amarelo") {
+        console.log("verde");
+    } else if (cor1 === "azul" && cor2 === "vermelho") {
+        console.log("roxo");
+    } else if (cor1 === "amarelo" && cor2 === "vermelho") {
+        console.log("laranja");
+    } else if (cor1 === "amarelo" && cor2 === "azul") {
+        console.log("verde");
+    } else if (cor1 === "vermelho" && cor2 === "azul") {
+        console.log("roxo");
+    } else if (cor1 === "vermelho" && cor2 === "amarelo") {
+        console.log("laranja");
+    }
+}
